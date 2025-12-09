9b51ef3af206d6aac50df3f21bf1342b038226f3
Anzony <37612034+iceheartzony@users.noreply.github.com>
2025-12-08 21:07:03 -0600

    fe
    fe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Importamos el Layout y las Páginas
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
@@ -10,23 +9,17 @@ import About from './pages/About';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Todas estas rutas compartirán el MainLayout (Navbar y Footer) */}
+      <Routes>    
         <Route path="/" element={<MainLayout />}>
-          
-          {/* Ruta página de inicio */}
+    
           <Route index element={<Home />} />
-          
-          {/* Ruta catálogo */}
+    
           <Route path="catalogo" element={<Catalog />} />
-          
-          {/* Ruta detalles */}
+   
           <Route path="producto/:id" element={<ProductDetail />} />
-          
-          {/* Ruta extra */}
+  
           <Route path="nosotros" element={<About />} />
-          
-          {/* Ruta 404  */}
+  
           <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
         
         </Route>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,7 @@
 const About = () => {
   return <h1>Sobre Nosotros</h1>;
+
+    <h1>Soy un estudiante de UNIR realizando su practica</h1>
+
 };
 export default About;
